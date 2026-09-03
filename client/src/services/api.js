621ab980ca1c489e5eca342f66b82b366eaef306
Manisha3024 <tolukattemanisha@gmail.com>
2026-09-03 11:11:39 +0530

    chnge url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:5000/api";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL=`${import.meta.env.VITE_API_URL || "http://localhost:5000"}/api`;
 async function request(endpoint, options = {}) {
     const url = `${BASE_URL}${endpoint}`;
     const config = {
